91370df3057b4569af222df8f14dfa556e6a0ad3
Bony Thomas <bothomas@tycoint.com>
2018-01-18 12:02:30 +0530

    Included node modules and auto launch code

--- js/freeboard.js
+++ js/freeboard.js
@@ -2847,7 +2847,7 @@ var freeboard = (function()
 
 			// Check to see if we have a query param called load. If so, we should load that dashboard initially
 			var freeboardLocation = getParameterByName("load");
-
+			
 			if(freeboardLocation != "")
 			{
 				$.ajax({
